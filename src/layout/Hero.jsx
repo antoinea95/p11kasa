71a9f4a065b24bd97fece8e9c216f96f129cedf7
Antoine <102359550+antoinea95@users.noreply.github.com>
2023-05-30 18:16:07 +0200

    create page and add styles on hero banner

---
+++ src/layout/Hero.jsx
@@ -0,0 +1,8 @@
+
+export default function Hero() {
+    return (
+        <section className="hero">
+                <h1 className="hero-title">Chez vous, partout et ailleurs</h1>
+        </section>
+    )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import CardFlat from "../composants/CardFlat";
+import Hero from "../layout/Hero";
+
+export default function Home({locations}) {
+
+    return (
+        <article className="home">
+        <Hero />
+        <section className="home_locations">
+            {locations.map(location => {
+                return  <Link  key={location.id} to={`/location/${location.id}`}><CardFlat location={location} /></Link>
+            })}
+        </section>
+        </article>
+    )
+}
