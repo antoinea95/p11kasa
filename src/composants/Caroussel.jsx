1f93add7b870f50cbff6cd93c39ec4cd3692a33e
Antoine <102359550+antoinea95@users.noreply.github.com>
2023-06-01 11:12:56 +0200

    add numerotation on caroussel componant

--- src/composants/Caroussel.jsx
+++ src/composants/Caroussel.jsx
@@ -1,45 +1,50 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export default function Caroussel({pictures}) {
-    
+export default function Caroussel({ pictures }) {
+  const [indexOfPicture, setIndexOfPicture] = useState(0);
 
-    const [indexOfPicture, setIndexOfPicture] = useState(0);
-
-    const handleNextPicture = () => {
-        if (indexOfPicture === pictures.length - 1) {
-            setIndexOfPicture(0);
-        } else {
-            setIndexOfPicture(previousIndex => previousIndex + 1);
-        }
+  const handleNextPicture = () => {
+    if (indexOfPicture === pictures.length - 1) {
+      setIndexOfPicture(0);
+    } else {
+      setIndexOfPicture((previousIndex) => previousIndex + 1);
     }
+  };
 
-    const handlePreviousPicture = () => {
-        if (indexOfPicture === 0) {
-            setIndexOfPicture(pictures.length - 1);
-        } else {
-            setIndexOfPicture(previousIndex => previousIndex - 1);
-        }
+  const handlePreviousPicture = () => {
+    if (indexOfPicture === 0) {
+      setIndexOfPicture(pictures.length - 1);
+    } else {
+      setIndexOfPicture((previousIndex) => previousIndex - 1);
     }
+  };
 
-    return (
-        <section className="caroussel">
-            <button 
-            className="caroussel-arrow" 
-            aria-label="Button pour revenir à l'image précédente" 
+  return (
+    <section className="caroussel">
+      <div className="caroussel-container">
+        <img src={pictures[indexOfPicture]} alt="" />
+      </div>
+      {pictures.length > 1 && (
+        <div className="caroussel-controllers">
+          <button
+            className="caroussel-arrow"
+            aria-label="Button pour revenir à l'image précédente"
             onClick={handlePreviousPicture}
-            > 
-                <img src="/assets/arrow-left.svg" alt="Flèche précédente" />
-            </button>
-            <div className="caroussel-container">
-            <img src={pictures[indexOfPicture]} alt="" />
-            </div>
-            <button 
-            className="caroussel-arrow" 
+          >
+            <img src="/assets/arrow-left.svg" alt="Flèche précédente" />
+          </button>
+          <span className="caroussel-numerotation">
+            {indexOfPicture + 1} / {pictures.length}
+          </span>
+          <button
+            className="caroussel-arrow"
             aria-label="Button pour passer à l'image suivante"
             onClick={handleNextPicture}
-            > 
-                <img src="/assets/arrow-right.svg" alt="Flèche suivante" />
-            </button>
-        </section>
-    )
+          >
+            <img src="/assets/arrow-right.svg" alt="Flèche suivante" />
+          </button>
+        </div>
+      )}
+    </section>
+  );
 }
